d1e8a7bd93140a558d194b3676b343de876a5c21
Jan Willem Keizer <janwillem@janwillemkeize.nl>
2025-04-18 17:56:56 +0200

    Initial commit

---
+++ src/main/java/nl/janwillemkeizer/personservice/dto/AlbumDto.java
@@ -0,0 +1,43 @@
+package nl.janwillemkeizer.personservice.dto;
+
+import java.util.List;
+
+public class AlbumDto {
+    private Long id;
+    private Long userId;
+    private String title;
+    private List<PhotoDto> photos;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<PhotoDto> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<PhotoDto> photos) {
+        this.photos = photos;
+    }
+} 
---
+++ src/main/java/nl/janwillemkeizer/personservice/dto/PostDto.java
@@ -0,0 +1,52 @@
+package nl.janwillemkeizer.personservice.dto;
+
+import java.util.List;
+
+public class PostDto {
+    private Long id;
+    private Long userId;
+    private String title;
+    private String body;
+    private List<CommentDto> comments;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public List<CommentDto> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDto> comments) {
+        this.comments = comments;
+    }
+} 
